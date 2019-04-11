c41802e5b371390577105f388665a2f026a438c9
Jordan Fraughton <i25203@dxvln1q1f8jc.xactware.com>
2019-04-11 13:13:30 -0600

    Summary

--- jsonScript.py
+++ jsonScript.py
@@ -8,36 +8,34 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 def makeWorkbook():
     #Set up Workbook
     workbook = xlsxwriter.Workbook('FloorPlan.xlsx')
-    #TODO Set up Summary
-    #loop through files
     files = getFilePath()
     number = 1
-    floorPlans = 0
+    floorPlans = []
+    percent_fmt = workbook.add_format({'num_format': '0.00%'})
+    bold_percent_fmt = workbook.add_format({'num_format': '0.00%', 'bold': True})
+    bold = workbook.add_format({'bold': True})
     for file in files:
         #Worksheet set up
         floorPlanList = makeFloorPlanList(file)
-        floorPlans += 1
         orthoWallsList = getWalls("orthorectified", floorPlanList)
         correctWallsList = getWalls("correctedMeasurment", floorPlanList)  
         if len(orthoWallsList) != len(correctWallsList):
             correctWallsList = fixCorrectedWallsList(correctWallsList)
         name = "FP" + str(number)
+        floorPlans.append(name)
         number += 1
-        makeWorksheet(name, workbook, floorPlanList, orthoWallsList, correctWallsList)
-    #Wall Groups For Summary
-         
+        makeWorksheet(name, workbook, floorPlanList, orthoWallsList, correctWallsList, percent_fmt, bold, bold_percent_fmt)     
     #Make Summary
-   
+    summaryWorksheet(workbook, percent_fmt, bold_percent_fmt, bold, floorPlans)
     #Close workbook
+    print(len(floorPlans))
     workbook.close()
 
-def makeWorksheet(name, workbook, floorPlanList, orthoWallsList, correctWallsList):
+def makeWorksheet(name, workbook, floorPlanList, orthoWallsList, correctWallsList, percent_fmt, bold, bold_percent_fmt):
     print(name)
     worksheet = workbook.add_worksheet(name)
     worksheet.set_column(0,0,29.5)
-    percent_fmt = workbook.add_format({'num_format': '0.00%'})
-    bold_percent_fmt = workbook.add_format({'num_format': '0.00%', 'bold': True})
-    bold = workbook.add_format({'bold': True})
+    
     formatExcel(worksheet, floorPlanList, orthoWallsList, correctWallsList, percent_fmt, bold, bold_percent_fmt)
    
 def formatExcel(worksheet, floorPlanList, orthoWallsList, correctWallsList, percent_fmt, bold, bold_percent_fmt):
@@ -60,63 +58,184 @@ def formatExcel(worksheet, floorPlanList, orthoWallsList, correctWallsList, perc
     #Average Difference in Inches Wall Groups
     worksheet.write('A11', 'Average difference in inches - all walls', bold)
     worksheet.write_formula('B11', '=SUM(B6:CA6)/COUNT(B6:CA6)', bold)
+    worksheet.write('D11', 'Walls', bold)
     worksheet.write('A12', 'Average difference < 5 feet walls (in)')
-    worksheet.write_formula('B12', '=SUMIFS(B6:CA6,B5:CA5,"<5")/COUNTIF(B5:CA5,"<5")')
+    worksheet.write_formula('B12', '=IFERROR(SUMIFS(B6:CA6,B5:CA5,"<5")/COUNTIF(B5:CA5,"<5"), "NA")')
+    worksheet.write_formula('D12', 'COUNTIF(B5:CA5,"<5")')
     worksheet.write('A13', 'Average difference 5-15 feet walls (in)')
-    worksheet.write_formula('B13', '=SUMIFS(B6:CA6,B5:CA5,">5",B5:CA5,"<15")/COUNTIFS(B5:CA5,">5",B5:CA5,"<15")')
+    worksheet.write_formula('B13', '=IFERROR(SUMIFS(B6:CA6,B5:CA5,">5",B5:CA5,"<15")/COUNTIFS(B5:CA5,">5",B5:CA5,"<15"), "NA")')
+    worksheet.write_formula('D13', 'COUNTIFS(B5:CA5,">5",B5:CA5,"<15")')
     worksheet.write('A14', 'Average difference 15-25 feet walls (in)')
-    worksheet.write_formula('B14', '=SUMIFS(B6:CA6,B5:CA5,">15",B5:CA5,"<25")/COUNTIFS(B5:CA5,">15",B5:CA5,"<25")')
+    worksheet.write_formula('B14', '=IFERROR(SUMIFS(B6:CA6,B5:CA5,">15",B5:CA5,"<25")/COUNTIFS(B5:CA5,">15",B5:CA5,"<25"), "NA")')
+    worksheet.write_formula('D14', 'COUNTIFS(B5:CA5,">15",B5:CA5,"<25")')
     worksheet.write('A15', 'Average difference 25 > feet walls (in)')
-    worksheet.write_formula('B15', '=SUMIFS(B6:CA6,B5:CA5,">25")/COUNTIF(B5:CA5,">25")')
+    worksheet.write_formula('B15', '=IFERROR(SUMIFS(B6:CA6,B5:CA5,">25")/COUNTIF(B5:CA5,">25"), "NA")')
+    worksheet.write_formula('D15', 'COUNTIF(B5:CA5,">25")')
     #Average Difference In % Wall Groups
     worksheet.write('A17','Average % difference - all walls', bold)
     worksheet.write_formula('B17', '=SUM(B7:CA7)/COUNT(B7:CA7)', bold_percent_fmt)
     worksheet.write('A18','Average % difference <5 feet walls')
-    worksheet.write_formula('B18', '=SUMIFS(B7:CA7,B5:CA5,"<5")/COUNTIFS(B5:CA5,"<5")', percent_fmt)
+    worksheet.write_formula('B18', '=IFERROR(SUMIFS(B7:CA7,B5:CA5,"<5")/COUNTIFS(B5:CA5,"<5"), "NA")', percent_fmt)
+    worksheet.write_formula('D18', 'COUNTIFS(B5:CA5,"<5")')
     worksheet.write('A19','Average % difference 5-15 feet walls')
-    worksheet.write_formula('B19', '=SUMIFS(B7:CA7,B5:CA5,">5",B5:CA5,"<15")/COUNTIFS(B5:CA5,">5",B5:CA5,"<15")', percent_fmt)
+    worksheet.write_formula('B19', '=IFERROR(SUMIFS(B7:CA7,B5:CA5,">5",B5:CA5,"<15")/COUNTIFS(B5:CA5,">5",B5:CA5,"<15"), "NA")', percent_fmt)
+    worksheet.write_formula('D19', 'COUNTIFS(B5:CA5,">5",B5:CA5,"<15")')
     worksheet.write('A20','Average % difference 15-25 feet walls')
-    worksheet.write_formula('B20', '=SUMIFS(B7:CA7,B5:CA5,">15",B5:CA5,"<25")/COUNTIFS(B5:CA5,">15",B5:CA5,"<25")', percent_fmt)
+    worksheet.write_formula('B20', '=IFERROR(SUMIFS(B7:CA7,B5:CA5,">15",B5:CA5,"<25")/COUNTIFS(B5:CA5,">15",B5:CA5,"<25"), "NA")', percent_fmt)
+    worksheet.write_formula('D20', 'COUNTIFS(B5:CA5,">15",B5:CA5,"<25")')
     worksheet.write('A21','Average % difference >25 feet walls')
-    worksheet.write_formula('B21', '=SUMIFS(B7:CA7,B5:CA5,">25")/COUNTIF(B5:CA5,">25")', percent_fmt)
+    worksheet.write_formula('B21', '=IFERROR(SUMIFS(B7:CA7,B5:CA5,">25")/COUNTIF(B5:CA5,">25"), "NA")', percent_fmt)
+    worksheet.write_formula('D21', 'COUNTIF(B5:CA5,">25")')
     worksheet.write('A23','Weighted % Difference Average', bold)
     worksheet.write_formula('B23', '=SUM(B8:CA8)', bold_percent_fmt)
 
-def summaryWorksheet(workbook, totalRoomCount, totalWallCount, weightedPercentageList, averageDifferenceList, floorPlans, groupsAvgListsList):
+def summaryWorksheet(workbook, percent_fmt, bold_percent_fmt, bold, floorPlans):
     worksheet = workbook.add_worksheet('Summary')
     worksheet.set_column(0,0,32)
     #Number of Floor Plans
     worksheet.write('A1', 'Number of Floor Plans')
-    worksheet.write('B1', floorPlans)
+    worksheet.write('B1', len(floorPlans))
     #Number of Walls
-    worksheet.write('A2', 'Number of Walls')
-    worksheet.write('B2', totalWallCount)
+    worksheet.write('A2', 'Number of Rooms')
+    worksheet.write('B2', getFormulaStringForSummary(floorPlans, "B2"))
     #Number of Rooms
-    worksheet.write('A3', 'Number of Rooms')
-    worksheet.write('B3', totalRoomCount)
-    #Average Difference in Inches
-    worksheet.write('A4', 'Average Differnce in Inches per Wall')
+    worksheet.write('A3', 'Number of Walls')
+    worksheet.write('B3', getFormulaStringForSummary(floorPlans, "B3"))
+    #Average Difference in Inches 
+    worksheet.write('A5', 'Average difference in inches - all walls', bold)
+    worksheet.write('B5', getFormulaStringForSummary(floorPlans, "B5"), bold)
+    worksheet.write('A6', 'Average difference < 5 feet walls (in)')
+    worksheet.write('B6', getFormulaStringForSummary(floorPlans, "B6"))
+    worksheet.write('A7', 'Average difference 5-15 feet walls (in)')
+    worksheet.write('B7', getFormulaStringForSummary(floorPlans, "B7"))
+    worksheet.write('A8', 'Average difference 15-25 feet walls (in)')
+    worksheet.write('B8', getFormulaStringForSummary(floorPlans, "B8"))
+    worksheet.write('A9', 'Average difference 25 > feet walls (in)')
+    worksheet.write('B9', getFormulaStringForSummary(floorPlans, "B9"))
     #Average Weighted Percentage Difference
-    worksheet.write('A5', 'Weighted Percentage Difference per Wall')
-    percent_fmt = workbook.add_format({'num_format': '0.00%'})
-    # Wall < 5
-    worksheet.write('A6', 'Walls < 5ft Avg Difference per Wall')
-    # Wall < 5 > 15
-    worksheet.write('A7', '5ft < Walls < 15ft Avg Difference per Wall')
-    # Wall < 15 > 25
-    worksheet.write('A8', '15ft < Walls < 25ft Avg Difference per Wall')
-    # Wall > 25
-    worksheet.write('A9', '25ft < Walls Avg Difference per Wall')
-    return 0
+    worksheet.write('A11', 'Average % difference - all walls', bold)
+    worksheet.write('B11', getFormulaStringForSummary(floorPlans, "B11"), bold_percent_fmt)
+    worksheet.write('A12', 'Average % difference <5 feet walls')
+    worksheet.write('B12', getFormulaStringForSummary(floorPlans, "B12"), percent_fmt)
+    worksheet.write('A13', 'Average % difference 5-15 feet walls')
+    worksheet.write('B13', getFormulaStringForSummary(floorPlans, "B13"), percent_fmt)
+    worksheet.write('A14', 'Average % difference 15-25 feet walls')
+    worksheet.write('B14', getFormulaStringForSummary(floorPlans, "B14"), percent_fmt)
+    worksheet.write('A15', 'Average % difference 25 > feet walls')
+    worksheet.write('B15', getFormulaStringForSummary(floorPlans, "B15"), percent_fmt)
+    worksheet.write('A17', 'Weighted % Difference Average', bold)
+    worksheet.write('B17', getFormulaStringForSummary(floorPlans, "B17"), bold_percent_fmt)
+    #Number of Walls
+    worksheet.write('D5', 'Walls', bold)
+    worksheet.write('D6', getFormulaStringForSummary(floorPlans, "D6"))
+    worksheet.write('D7', getFormulaStringForSummary(floorPlans, "D7"))
+    worksheet.write('D8', getFormulaStringForSummary(floorPlans, "D8"))
+    worksheet.write('D9', getFormulaStringForSummary(floorPlans, "D9"))
+    worksheet.write('D12', getFormulaStringForSummary(floorPlans, "D12"))
+    worksheet.write('D13', getFormulaStringForSummary(floorPlans, "D13"))
+    worksheet.write('D14', getFormulaStringForSummary(floorPlans, "D14"))
+    worksheet.write('D15', getFormulaStringForSummary(floorPlans, "D15"))
 
+#Summary Functions
 
+def getFormulaStringForSummary(floorPlans, cell):
+    formulaString = ""
+    #Counts
+    if cell == "B2":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B1,"
+    elif cell == "B3":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B2,"
+    #Average in inches
+    elif cell == "B5":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B11,"
+    elif cell == "B6":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B12,"
+    elif cell == "B7":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B13,"
+    elif cell == "B8":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B14,"
+    elif cell == "B9":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B15,"
+    #Average % difference
+    elif cell == "B11":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B17,"
+    elif cell == "B12":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B18,"
+    elif cell == "B13":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B19,"
+    elif cell == "B14":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B20,"
+    elif cell == "B15":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B21,"
+    elif cell == "B17":
+        formulaString = "=AVERAGE("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!B23,"
+    #Walls
+    elif cell == "D6":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D12,"
+    elif cell == "D7":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D13,"
+    elif cell == "D8":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D14,"
+    elif cell == "D9":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D15,"
+    elif cell == "D12":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D18,"
+    elif cell == "D13":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D19,"
+    elif cell == "D14":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D20,"
+    elif cell == "D15":
+        formulaString = "=SUM("
+        for floor in floorPlans:
+            formulaString = formulaString + "'" + floor + "'!D21,"
+        
+    formulaString = formulaString[:-1]
+    formulaString += ")"
+    return formulaString
+        
 
 #Wall Functions
 
-#def avgDifferenceInWallGroups():
-
-
-        
 def getWalls(type, floorPlanList): 
     wallsList = []
     for floor in floorPlanList:
@@ -246,6 +365,7 @@ def makeFloorPlanList(jsonPlan):
     floorPlanList = pyFloorPlan["floorPlans"]
     return floorPlanList
 
+#=SUM('FP1'!B11,'FP2'!B11,'FP3'!B11,'FP4'!B11,'FP5'!B11,'FP6'!B11,'FP7'!B11)/COUNT('FP1'!B11,'FP2'!B11,'FP3'!B11,'FP4'!B11,'FP5'!B11,'FP6'!B11,'FP7'!B11)
 #To remove ds.store files after placing new group of logs in folder run "find . -name '.DS_Store' -type f -delete" in terminal. 
 
 makeWorkbook()
